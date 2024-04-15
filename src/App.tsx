27bc7b469c8a38545f1fba44185f80804794399e
Adonai Romero <adonai@adonais-mbp.lan>
2024-04-15 13:39:26 -0600

    update app component with conditional rendering and rendering an array

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
 
 export default function App() {
-  return <h1>Greetings Earthlings</h1>
+  const firstName: string = 'Adonai';
+  const lastName: string = 'Romero';
+  const isLoggedIn: boolean = true;
+
+  const posts: {id: number, title: string}[] = [
+    {id: 1, title: 'Grounding Techniques'},
+    {id: 2, title: 'Smudging 101'},
+    {id: 3, title: 'Meditation for Beginners'},
+  ]
+  
+  return (
+    <>
+      <h1>Greetings Earthlings</h1>
+      <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
+      {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
+    </>
+  )
 }
