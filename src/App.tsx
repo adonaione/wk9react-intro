ebbe34487d3ea5476705b81c03090826d4238f04
Adonai Romero <adonai@adonais-mbp.lan>
2024-04-15 15:37:46 -0600

    Add useState and add click event to change state

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
+import { useState } from 'react';
 import Navigation from './components/Navigation';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button'; 
 
 
 export default function App() {
   const firstName: string = 'Adonai';
   const lastName: string = 'Romero';
-  const isLoggedIn: boolean = true;
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const posts: {id: number, title: string}[] = [
     {id: 1, title: 'Grounding Techniques'},
     {id: 2, title: 'Smudging 101'},
     {id: 3, title: 'Meditation for Beginners'},
   ]
+
+  
+  const handleClick = () => {
+    // console.log('The button has been clicked');
+    setIsLoggedIn(!isLoggedIn);
+  }
   
   return (
     <>
       <Navigation isLoggedIn={isLoggedIn}/>
       <Container>
         <h1>Greetings Earthlings</h1>
+        <Button variant='primary' onClick={handleClick}>Click Me!</Button>
         <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
         {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
         </Container>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import './main.css'; // Import our own CSS - Should be imported AFTER bootstrap CSS
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
