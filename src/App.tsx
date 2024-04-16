9347d5199eeda0f38297754dd8d5ef4da5cd6257
Adonai Romero <adonai@Adonais-MacBook-Pro.local>
2024-04-16 11:27:55 -0600

    add post search + nav background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import Navigation from './components/Navigation';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button'; 
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 type Post = {
   id: number,
@@ -10,14 +12,15 @@ type Post = {
 }
 
 type Sorting = {
-  idAsc: (a: Post, b:Post) => number,
-  idDesc: (a: Post, b:Post) => number,
-  titleAsc: (a: Post, b:Post) => number,
-  titleDesc: (a: Post, b:Post) => number,
+  idAsc: (a: Post, b: Post) => number,
+  idDesc: (a: Post, b: Post) => number,
+  titleAsc: (a: Post, b: Post) => number,
+  titleDesc: (a: Post, b: Post) => number,
 }
 
 
 export default function App() {
+  console.log('App component is rendering');
   const firstName: string = 'Adonai';
   const lastName: string = 'Romero';
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,47 +31,60 @@ export default function App() {
   //]
 
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Grounding Techniques'},
-    {id: 2, title: 'Smudging 101'},
-    {id: 3, title: 'Meditation for Beginners'},
-    {id: 4, title: 'The Power of Crystals'}
+    { id: 1, title: 'Grounding Techniques' },
+    { id: 2, title: 'Smudging 101' },
+    { id: 3, title: 'Meditation for Beginners' },
+    { id: 4, title: 'The Power of Crystals' }
   ]);
 
+  const [searchTerm, setSearchTerm] = useState('');
+
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        
+
     const sortFunctions = {
-      idAsc: (a:Post, b:Post) => a.id - b.id,
-      idDesc: (a:Post, b:Post) => b.id - a.id,
-      titleAsc: (a:Post, b:Post) => a.title > b.title ? 1 : -1,
-      titleDesc: (a:Post, b:Post) => b.title > a.title ? 1 : -1
+      idAsc: (a: Post, b: Post) => a.id - b.id,
+      idDesc: (a: Post, b: Post) => b.id - a.id,
+      titleAsc: (a: Post, b: Post) => a.title > b.title ? 1 : -1,
+      titleDesc: (a: Post, b: Post) => b.title > a.title ? 1 : -1
     }
     const func = sortFunctions[e.target.value as keyof Sorting];
     const newSortedArr = [...posts].sort(func);
     setPosts(newSortedArr);
-}
+  }
 
-  
+
   const handleClick = () => {
     // console.log('The button has been clicked');
     setIsLoggedIn(!isLoggedIn);
   }
- 
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
     <>
-      <Navigation isLoggedIn={isLoggedIn}/>
+      <Navigation isLoggedIn={isLoggedIn} />
       <Container>
         <h1>Greetings Earthlings</h1>
         <Button variant='primary' onClick={handleClick}>Click Me!</Button>
         <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
-        <Form.Select onChange={handleSelectChange}>
-          <option>Choose Sorting Option</option>
-          <option value="idAsc">Sort By ID ASC</option>
-          <option value="isDesc">Sort By ID DESC</option>
-          <option value="titleAsc">Sort By Title ASC</option>
-          <option value="titleDesc">Sort By Title DESC</option>
-        </Form.Select>
-        {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
-        </Container>
+        <Row>
+          <Col xs={12} md={8}>
+            <Form.Control value={searchTerm} placeholder='Search Posts' onChange={handleInputChange} />
+          </Col>
+          <Col>
+            <Form.Select onChange={handleSelectChange}>
+              <option>Choose Sorting Option</option>
+              <option value="idAsc">Sort By ID ASC</option>
+              <option value="idDesc">Sort By ID DESC</option>
+              <option value="titleAsc">Sort By Title ASC</option>
+              <option value="titleDesc">Sort By Title DESC</option>
+            </Form.Select>
+          </Col>
+        </Row>
+        {posts.filter(p => p.title.toLowerCase().startsWith(searchTerm.toLowerCase())).map( p => <h4 key={p.id}>{p.title}</h4> )}
+      </Container>
     </>
   )
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,22 +1,27 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 type NavigationProps = {
-    isLoggedIn: boolean;
+    isLoggedIn: boolean
 }
 
-export default function Navigation({isLoggedIn }: NavigationProps){
+export default function Navigation({ isLoggedIn }: NavigationProps){
+    console.log('Navigation component is being rendered');
 
+    const [backgroundTheme, setBackgroundTheme] = useState('dark');
+    
     return (
-        <Navbar expand='lg' data-bs-theme='dark' bg='dark'>
+        <Navbar expand='lg' data-bs-theme={backgroundTheme} bg={backgroundTheme}>
             <Container fluid>
                 <Navbar.Brand href='/'>Kekambas Blog</Navbar.Brand>
                 <Navbar.Toggle aria-controls='nav-collapse' />
-                <Navbar.Collapse id="nav-collapse">
+                <Navbar.Collapse id='nav-collapse'>
                     <Nav className='me-auto'>
                         {isLoggedIn ? (
-                           <>
+                            <>
                                 <Nav.Link href='/'>Create Post</Nav.Link>
                                 <Nav.Link href='/'>Log Out</Nav.Link>
                             </>
@@ -26,6 +31,9 @@ export default function Navigation({isLoggedIn }: NavigationProps){
                                 <Nav.Link href='/'>Log In</Nav.Link>
                             </>
                         )}
+                    </Nav>
+                    <Nav>
+                        <Button onClick={() => setBackgroundTheme(backgroundTheme === 'dark' ? 'light' : 'dark')}>Change Background</Button>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
